4ed2a80ec85d334560268818ef29af3c8355103e
MilenaMarija <53303LT@gmail.com>
2021-03-14 21:21:53 +0200

    2021-03-09

---
+++ js/00-notes.js
@@ -0,0 +1,15 @@
+// kintamieji - jų atmintyje yra laikoma informacija,..
+// sąlygos sakiniai - iššokanti logiką,pagal ką daryti kai daugiau/mažiau, tiesa/ne tiesa,..
+// ciklai - kai turime sąrašą su informacija, kuri yra vienodos struktūros.
+// funkcijos - aprašome logiką, kurią norime perpanaudoti N kartų, tik su galimai skirtingais parametrais.
+
+// event'ai (event listeners) - skirti stebėti įvykius (click, scroll, keypress ir pan.) į kuriuos reikia sureaguoti iškviečiant norimą funkciją.
+
+/*
+KINTAMŲJŲ INICIAVIMO BŪDAI:
+const - kintamasis, kurio reikšmės negalima iš naujo priskirti*
+let - kintamasis, kurio reikšmę galima priskirti kiek norite kartų iš naujo
+var - esant galimybei, NENAUDOTI!!! naudoti kitus
+
+* - yra papildomų sąlygų su kompleksiniais kintamaisiais
+*/
