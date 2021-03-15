4a47b62bae8d19b9042d2db8f462ef4c1aff7b1a
MilenaMarija <53303LT@gmail.com>
2021-03-15 10:02:40 +0200

    03-if pradzia

--- js/02-strings.js
+++ js/02-strings.js
@@ -2,5 +2,29 @@
 VISOS KOMBINACIJOS DARBUI SU TEKSTU
 */
 
+// Siandien yra: Kovo desimta.
 const menuo = 'Kovas';
 const diena = 'desimta';
+
+const dayString = 'Siandien yra: ' + menuo + ' ' + diena + '.';
+console.log(dayString);
+
+// Cia yra dviguba kabute (") kabute.
+const dvigubaKabute = 'Cia yra dviguba (") kabute.';
+console.log(dvigubaKabute);
+
+// Cia yra vienguba (') kabute.
+const viengubaKabute = "Cia yra vienguba (') kabute.";
+console.log(viengubaKabute);
+
+// Turiu vienguba (') ir dviguba (") kabutes.
+const viengubaDviguba = "Turiu vienguba (') ir dviguba (\") kabutes."
+console.log(viengubaDviguba);
+
+// Turiu dviguba (") ir vienguba (') kabutes. 
+const dvigubaVienguba = 'Turiu dviguba (") ir vienguba (\') kabutes.';
+console.log(dvigubaVienguba);
+
+// Max saugumas dviguba (") ir vienguba (') kabutes.
+const maxKabutes2 = "Max saugumas dviguba (\") ir vienguba (\') kabutes."
+console.log(maxKabutes2);
