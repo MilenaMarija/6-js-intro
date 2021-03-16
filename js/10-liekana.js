c11f46113ed11713cd690ccd9de59ff102cc8802
MilenaMarija <53303LT@gmail.com>
2021-03-16 11:20:11 +0200

    10-liekana pradžia

---
+++ js/10-liekana.js
@@ -0,0 +1,7 @@
+/*
+Suskaičiuoti kiek nurodytame intervale yra skaičių, kurie dalijasi 
+
+ruzultatą pateikti  tokiu formatu:
+Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 3 yra
+Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 5 yra
+Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 7 yra
