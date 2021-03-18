0f25cedf9c81106348a596b8befc505057a04224
MilenaMarija <53303LT@gmail.com>
2021-03-18 10:43:36 +0200

    15-digits pradžia

---
+++ js/15-digits.js
@@ -0,0 +1,10 @@
+function digits(skaicius) {
+    return 0;
+}
+
+console.log(digits(5), '->', 1);
+console.log(digits(5), '->', 1);
+console.log(digits(5), '->', 1);
+console.log(digits(5), '->', 1);
+console.log(digits(5), '->', 1);
+console.log(digits(5), '->', 1);
