c9467ab41bf40e7da74b4c2f06fbf299cedcf2c2
MilenaMarija <53303LT@gmail.com>
2021-03-20 00:55:11 +0200

    04 savaites diena

--- js/04-swich.js
+++ js/04-swich.js
@@ -6,10 +6,98 @@ switch() { case, break, default}
 
 */
 
-const animal = 'dog';
+const animal = 'cat';
 
 switch (animal) {
     case 'dog':
         console.log('Bark bark');
         break;
+    
+    case 'cat':
+        console.log('Meow meow');
+        break;
+
+    default:
+        console.log('Unknown animal');
+        break;
 }
+
+if (animal === 'dog') {
+    console.log('Bark bark');
+ } else if (animal === 'cat') {
+    console.log('Meow meow');
+} else {
+    console.log('Unknown animal');
+}
+// Aukščiau esančias logikas galima pasirinktinai rašyti vieną arba kitą, nes jos analogiškos - reikšmės identiškos, tik užrašymo būdas skiriasi.
+console.log('---------------------');
+
+const day = 4;
+
+switch (day) {
+    case 1:
+        console.log('Pr');
+        break;
+    case 2:
+        console.log('An');
+        break;
+    case 3:
+        console.log('Tr');
+        break;
+    case 4:
+        console.log('Kt');
+        break;
+    case 5:
+        console.log('Pn');
+        break;
+    case 6:
+        console.log('Št');
+        break;
+    case 7:
+        console.log('Sk');
+        break;
+    default:
+        console.log('Tokia diena savaiteje neegzistuoja.');
+        break;
+}
+switch (day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log('Darbo diena.');
+        break;
+    case 6:
+    case 7:
+        console.log('Savaitgalio diena.');
+        break;
+    default:
+        console.log('Tokia diena savaiteje neegzistuoja..');
+        break;
+}
+switch (day) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+        console.log('Nelyginė diena.');
+        break;
+    case 2:
+    case 4:
+    case 6:
+        console.log('Lyginė diena.');
+        break;
+    default:
+        console.log('Tokia diena savaiteje neegzistuoja..');
+        break;
+}
+if (day === 1 || day === 2 || day === 3 || day === 4 || day === 5) {
+    console.log('Darbo diena..');
+} else if (day === 6 || day === 7) {
+    console.log('savaitgalio diena');
+} else {
+    console.log('Tokia diena savaiteje neegzistuoja');
+}
+console.log('---------------');
+
