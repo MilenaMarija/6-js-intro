133e556ef12152d3fbd2af7954734a6a53f90c87
MilenaMarija <53303LT@gmail.com>
2021-03-20 17:58:00 +0200

    06 markIndex3

--- js/06-math.js
+++ js/06-math.js
@@ -29,3 +29,46 @@ const average = sum / count;
 console.log(average);
 
 console.log('------------');
+
+const marks2 = [2, 4, 6, 8];
+
+let sum2 = 0;               
+console.log(sum2);
+
+sum2 += marks2[0];    // 0 + 2
+console.log(sum2);
+
+sum2 += marks2[1];    // 2 + 4
+console.log(sum2);
+
+sum2 += marks2[2];    // 6 + 6
+console.log(sum2);
+
+sum2 = sum2 + marks2[3];    // 12 + 8
+console.log(sum2);
+
+console.log('------------');
+
+const marks3 = [2, 4, 6, 8];
+
+let sum3 = 0;
+let markIndex3 = 0;               
+console.log(sum3);
+
+sum3 += marks3[markIndex3];    // 0 + 2
+console.log(sum3);
+
+markIndex3 += 1;             // 2 + 4
+sum3 += marks3[markIndex3];
+console.log(sum3);
+
+markIndex3 += 1;               // 6 + 6
+sum3 += marks3[markIndex3];
+console.log(sum3);
+
+markIndex3 += 1;              // 12 + 8
+sum3 += marks3[markIndex3];
+console.log(sum3);
+
+console.log('------------');
+
