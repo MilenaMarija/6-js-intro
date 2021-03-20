761249135d725348c10a1a26dbf16dd39ee50244
MilenaMarija <53303LT@gmail.com>
2021-03-20 21:14:49 +0200

    07 ciklas for

--- js/07-for.js
+++ js/07-for.js
@@ -8,7 +8,7 @@ for () {}
 
 
 
-for (let i = 1; i <= 3; i++) {
+for (let i = 3; i <= 7; i++) {
     console.log(i);
 } 
 
@@ -16,3 +16,17 @@ console.log('ciklo pabaiga');
 
 console.log('---------------');
 
+const abc = ['r', 'y', 't', 'a', 's'];
+
+let zodis = '';
+let abcIndex = 0;
+
+zodis += abc[abcIndex++];
+zodis += abc[abcIndex++];
+zodis += abc[abcIndex++];
+zodis += abc[abcIndex++];
+zodis += abc[abcIndex++];
+
+console.log(zodis);
+
+console.log('---------');
