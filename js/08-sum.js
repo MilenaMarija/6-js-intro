1146d9c6b7c67f83ca84bd5d2e7df7d3468fe660
MilenaMarija <53303LT@gmail.com>
2021-03-21 22:22:36 +0200

    08-sum uzdaviniu sprendimo pr

--- js/08-sum.js
+++ js/08-sum.js
@@ -1,3 +1,22 @@
 /*
-Suskaičiuoti ką gausime susumavus skaičius intervale tarp (imtinai)
+1. Suskaičiuoti ką gausime susumavus skaičius intervale tarp (imtinai):
+
+a. 0 - 0        = 0
+b. 0 - 4        = 10
+c. 0 - 100      = 5050
+d. 574 - 815    = labai didelis skaicius
+e. -50 - 50     = 0
+f. -70 - 30     = santykinai didelis neigiamas skaicius (-2020)
+
 */
+// b.
+const nuo = 0;
+const iki = 4;
+let suma = 0;
+
+for (let i = nuo ; i <= iki; i++) {
+    suma += i;
+}
+
+const ats = 'Intervale nuo ' + nuo + iki + ' suma yra ' + suma + '.';
+console.log(ats);
