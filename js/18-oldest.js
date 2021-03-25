cabde0f1638f5f5ba0f5c1827b4dcd36788028db
MilenaMarija <53303LT@gmail.com>
2021-03-25 11:19:00 +0200

    18-oldest nebaigtas

--- js/18-oldest.js
+++ js/18-oldest.js
@@ -2,10 +2,24 @@
 Gimines medyje reikai rasti kas buvo/yra vyriausias?
 */
 
-functionoldest() {
-    return 0;
+function oldest(asmuo) {
+    return giminesSaka.age;
 }
 
 const gimine1 = {
-    
+    name: 'Petras',
+    age 40,
+    children: [
+        {
+            name: 'Maryte',
+            age: 45
+        },
+        {
+            name: 'Jonas',
+            age: 43
+        },
+    ]
 }
+
+const vyriausias = oldest(gimine1);
+console.log(vyriausias);
