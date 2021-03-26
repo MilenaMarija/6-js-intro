7b00a7f2361e3dbaa8fcfd67889a230b66df65b6
MilenaMarija <53303LT@gmail.com>
2021-03-26 23:07:55 +0200

    10-remainder papildyta

--- js/10-liekana.js
+++ js/10-liekana.js
@@ -7,7 +7,7 @@ Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 5 yra 3 viene
 Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 7 yra 2 vienetai.
 */
 
-const nuo = 1;
+const nuo = 0;
 const iki = 11;
 const daliklis = 3;
 let kiekis = 0;
@@ -21,3 +21,22 @@ for (let i = nuo; i <= iki; i++) {
 const ats = `Skaičių intervale tarp ${nuo} ir ${iki}, besidalijančių be liekanos iš ${daliklis} yra ${kiekis} vienetai.`;
 
 console.log(ats);
+
+/*
+Norint padalinti skaičius nuo 0 iki 20 iš 5, be liekanos:
+0 - 20 / 5
+Ats.: 0, 5, 10, 15, 20 - tai yra 
+i+=5 tai yra i+=daliklis
+----------------
+Norint padalinti skaičius nuo 1 iki 20 iš 5, be liekanos:
+1 - 20 / 5
+Ats.: 5, 10, 15, 20 - reiktų ciklą pradėti nuo artimiausio skaičiaus, kuris dalinasi be liekanos.
+------------
+Norint padalinti skaičius nuo 11 iki 20 iš 5, be liekanos:
+11 - 20 / 5
+Radus pirmą skaičių, kuris yra tinkamas, jeigu žinote pabaigą, užtektų eiti per kažkelintą skaičių, sutaupant daug laiko ir surasti tinkamą atsakymą, pvz. 
+i+=5 tai yra i+=daliklis
+Ats.: 15, 20 
+(20-15) / 5 + 1 tai yra supaprastinta formulė, kuri yra lygi 
+(iki-nuo) / daliklis + 1
+*/
