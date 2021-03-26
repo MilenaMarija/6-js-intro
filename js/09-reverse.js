e95de30bbdf7313e4f356c757f189627d992c1d1
MilenaMarija <53303LT@gmail.com>
2021-03-26 15:02:38 +0200

    09-reverse

--- js/09-reverse.js
+++ js/09-reverse.js
@@ -1,4 +1,14 @@
+/*
+2. panaudojant ciklą perrašyti tekstinio tipo kintamųjų reikšmes iš kito galo:
+pvz.: “abcdef” -> “fedcba”
+*/
 
+const text = 'abcdef';
+let reverse = '';
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
 
 
 
