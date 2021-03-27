ebee29c96c004a9dba83d45501900b56259d3d13
MilenaMarija <53303LT@gmail.com>
2021-03-27 23:13:49 +0200

    11-suma

--- js/11-suma.js
+++ js/11-suma.js
@@ -1,21 +1,45 @@
 const a = 8;
 const b = 5;
 
-const s = a +b;
-console.log(s);
-
-
-
 const c = 99;
 const d = 88;
 
-const s2 = c + d + 5;
+const e = 2;
+const f = 3;
+
+
+function desimteriopaSuma(pirmas, antras) {
+    const s = (pirmas + antras) * 10;
+    console.log(s);
+}
+
+desimteriopaSuma(e, f);
+console.log('--------');
+desimteriopaSuma(a, b);
+console.log('--------');
+desimteriopaSuma(c, d);
 
 
 
 
-const e = 54155;
-const f = 89514;
 
-const s3 = (e + f) * 10;
-console.log(s3);
+function labas(vardas, amzius, vietovadis) {
+    console.log(`Labas, as esu ${vardas}, man ${amzius} metai ir gyvenu ${vietovadis}.`);
+}
+
+labas('Petras', 88, 'vilniuje');
+labas('Maryte', 77, 'Kaune');
+labas('Alphe', 5, 'namie');
+
+
+
+
+// f(x) = x*x;
+
+function kvadaratu(x) {
+    console.log(x * x);
+}
+
+kvadaratu(5)
+kvadaratu(55)
+kvadaratu(5554845)
